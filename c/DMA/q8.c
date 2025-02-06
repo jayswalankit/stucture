ec1bb54e3b36508e732c4dca010c3e05711bb6f2
ankit <jayswalankit94@gmail.com>
2025-02-06 22:43:09 +0530

    dsa question practices

--- c/DMA/q8.c
+++ c/DMA/q8.c
@@ -10,18 +10,18 @@ int main(){
     for(int i=0;i<n;i++){
     scanf("%d",&arr[i]);
 }
-int n1;
-    printf("enter your size");
-    scanf("%d",&n1);
-int newsize=n+n1;
+// int n1=n;
+    // printf("enter your size");
+    // scanf("%d",&n1);
+int newsize=n;
 arr=(int*)realloc(arr,newsize*sizeof(int));
    double result=0;
-    printf("enter your new number");
-    for(int i=n;i<newsize;i++){
-    scanf("%d",&arr[i]);
-}
+    // printf("enter your new number");
+    // for(int i=n;i<newsize;i++){
+    // scanf("%d",&arr[i]);
+// }
     for(int i=0;i<newsize;i++){
-        result=sqrt(arr[i]);
+        result=sqrt(arr[i-n]);
     printf("%lf\n",result);
 }
 }
