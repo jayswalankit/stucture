24f4c0ed71dc41d9505136edda5094b93648b814
ankit <jayswalankit94@gmail.com>
2025-03-01 14:23:25 +0530

    updation

---
+++ c/file handling/updation.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct student{
+    char name[50];
+    int rollno;
+    float marks;
+} stu;
+stu s1[2];
+int main(){
+    FILE*file=fopen("update.txt","r");
+    FILE*file2=fopen("tem.txt","a");
+    
+    for(int i=0;i<2;i++){
+        
+        printf("enter youur name");
+      fgets(s1[i].name,50,stdin);
+      printf("ente ryour rollno");
+      scanf("%d",&s1[i].rollno);
+      printf("enter your marks");
+      scanf("%f",&s1[i].marks);
+      getchar();
+      
+      fprintf(file2,"%s %d %f",s1[i].name,s1[i].rollno,s1[i].marks);
+    }
+fclose(file);
+fclose(file2);
+
+remove("update.txt");
+rename("tem.txt","update.txt");
+
+
+
+file=fopen("update.txt","r");
+for(int i=0;i<2;i++){
+
+    printf("%s %d %f\n",s1[i].name,s1[i].rollno,s1[i].marks);
+
+}
+
+}
